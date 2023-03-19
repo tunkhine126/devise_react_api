8196a514998349e607c33b59106573c514bee573
Tun Khine <tunkhine126@gmail.com>
2023-03-19 14:02:08 -0400

    Configure auth

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(_resource, _opts = {})
     render json: {
-      message: "You aren't logged in.",
+      message: "You are logged in.",
       user: current_user
     }, status: :ok
   end
